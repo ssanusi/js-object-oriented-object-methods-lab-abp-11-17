44f2b42831b7b36d6af2bd78d427db7d8b27a7d0
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 01:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function BoardMember(name,homeState,training) {
   this.training = training;
   this.veto = function () {
     return `No, I must disagree`;
-    
+
   }
   this.approve = function () {
-    return `You can do that!`;    
+    return `You can do that!`;
   }
   this.doCharity = function (){
     return `I like to help people.`;
   }
   this.releasePressStatement = function () {
     return `You will see great things from Scuber.`;
-    
+
   }
   this.sayHi = function () {
-    return `"Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
-    
+    return `"Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+
   }
 }
